1c86eb27e9a1efb1373e8524d3fb3639886355b7
dversoza <daniel.ve3@gmail.com>
2020-09-25 18:23:13 -0300

    Added Power BI data visualization

--- pages/inventory.js
+++ pages/inventory.js
@@ -14,9 +14,9 @@ const New = () => {
   return (
     <>
       <iframe
-        width="100%"
-        height="800"
-        src="https://app.powerbi.com/reportEmbed?reportId=946001bf-78ae-48d9-b06c-8a57854c886b&autoAuth=true&ctid=1b310b5d-cc68-438e-b126-5d6814c9152f&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWJyYXppbC1zb3V0aC1iLXByaW1hcnktcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D"
+        width="600"
+        height="373.5"
+        src="https://app.powerbi.com/view?r=eyJrIjoiNTE3ZDNkOWQtZjkyMy00NzMwLTgwNWYtZjhkNDIxNmZjMmExIiwidCI6IjFiMzEwYjVkLWNjNjgtNDM4ZS1iMTI2LTVkNjgxNGM5MTUyZiJ9"
         frameborder="0"
         allowFullScreen="true"
       ></iframe>
